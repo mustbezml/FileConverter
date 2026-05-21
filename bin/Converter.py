0f003206f3dbaea03221552b39a653b00834f67d
ZmL <mustbezml@gmail.com>
2026-05-21 23:22:06 +0300

    Created basic structure

---
+++ bin/Converter.py
@@ -0,0 +1,6 @@
+import json
+import xml.etree.ElementTree as ET
+
+class FileConverter:
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from bin import *
+
+if __name__ == "__main__":
+    ...
